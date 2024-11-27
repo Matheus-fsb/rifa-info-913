2e22caefceaa50cc796354b7ec71bdfcf5341fd6
Matheus Barros <mfsb010507@gmail.com>
2024-11-27 15:10:55 -0300

    add port

--- src/server.js
+++ src/server.js
@@ -1,15 +1,16 @@
 // Importa a aplicação e a configuração do banco de dados
 const app = require('./app');
 const sequelize = require('./config/database');
+require('dotenv').config();
 
-const PORT = 3000;
+const port = process.env.PORT;
 
 // Sincroniza o banco de dados com a aplicação
 sequelize.sync({ force: false })
   .then(() => {
     // Inicia o servidor quando o banco de dados for sincronizado com sucesso
-    app.listen(PORT, () => {
-      console.log(`Servidor iniciado em: ${PORT}`);
+    app.listen(port, () => {
+      console.log(`Servidor iniciado em: ${port}`);
       console.log('Banco de dados sincronizado com sucesso.');
     });
   })
