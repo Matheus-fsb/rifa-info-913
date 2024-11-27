793db4998b622548aac1e0128ade909485f842de
Matheus Barros <mfsb010507@gmail.com>
2024-11-26 21:11:27 -0300

    tudo funcionando legal!!!

--- public/scripts-adm/log.js
+++ public/scripts-adm/log.js
@@ -4,14 +4,23 @@ const computer = document.getElementById('pc');
 
 computer.addEventListener('click', async (event) => {
     try {
-        const response = await fetch('http://localhost:3000/log'); // Obtém o log do backend
+        // Faz a requisição para obter o log do backend
+        const response = await fetch('http://localhost:3000/log', {
+            method: 'GET',
+            headers: createAuthHeaders(),
+        });
+
+        if (!response.ok) {
+            throw new Error('Erro ao obter o log do servidor');
+        }
+
         const log = await response.json();
 
         // Envia o log para o endpoint que salva o arquivo
         const saveResponse = await fetch('http://localhost:3000/save-log', {
             method: 'POST',
             headers: createAuthHeaders(),
-            body: JSON.stringify(log)
+            body: JSON.stringify(log),
         });
 
         if (saveResponse.ok) {
